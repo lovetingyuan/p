0fbf554606845eb89bccd0c605d8093ba5db9278
黄百途 <huangbaitu@meituan.com>
2022-01-29 20:07:58 +0800

    Update hfjhfeofakdkals23423423.js

--- hfjhfeofakdkals23423423.js
+++ hfjhfeofakdkals23423423.js
@@ -1,8 +1,7 @@
 const input = document.getElementById('input')
 const button = document.getElementById('button')
-const result = document.getElementById('result')
 
-async function sha256(str) {
+async function en(str) {
   const buf = await crypto.subtle.digest('SHA-512', new TextEncoder('utf-8').encode(str))
   return Array.prototype.map.call(new Uint8Array(buf), (x, i) => {
     if (x === 255) {
@@ -14,22 +13,20 @@ async function sha256(str) {
     if (x < 32) {
       x += (32 + i)
     }
-    if (!(x >= 32 && x <= 126)) {
-      console.assert(false)
-    }
     return String.fromCharCode(x)
   }).join('')
 }
 
 button.addEventListener('click', async () => {
   const val = input.value
+  if (!val) return
   if (button.val !== val || button.len === 8) {
     button.len = 64
   } else {
     button.len /= 2;
   }
   button.val = val
-  const p = await sha256(val)
+  const p = await en(val)
   await navigator.clipboard.writeText(p.substr(0, button.len))
   button.style.color = 'green'
 })
